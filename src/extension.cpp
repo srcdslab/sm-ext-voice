2aa49195f66a36e431567b88d3f1a9ba6ab6dc5b
Dolly <109222243+Dolly132@users.noreply.github.com>
2026-09-12 11:49:48 +0300

    Fully fix voice for css on windows

--- src/extension.cpp
+++ src/extension.cpp
@@ -492,7 +492,7 @@ bool convert_ip(const char *ip, struct in_addr *addr)
 #endif
 }
 
-void close_socket(int sock)
+void close_socket(socket_t sock)
 {
 #ifdef _WIN32
     closesocket(sock);
@@ -504,8 +504,6 @@ void close_socket(int sock)
 int my_poll(struct pollfd *fds, int nfds, int timeout)
 {
 #ifdef _WIN32
-	// Define nfds_t on Windows if needed
-	typedef int nfds_t;
 	return WSAPoll(fds, nfds, timeout);
 #else
 	return poll(fds, nfds, timeout);
@@ -515,9 +513,12 @@ int my_poll(struct pollfd *fds, int nfds, int timeout)
 int my_ioctl(socket_t sockfd, long cmd, size_t *argp)
 {
 #ifdef _WIN32
-    return ioctlsocket(sockfd, cmd, reinterpret_cast<u_long*>(argp)); // Windows version
+    u_long avail = 0;
+    int ret = ioctlsocket(sockfd, cmd, &avail);
+    *argp = avail;
+    return ret;
 #else
-    return ioctl(sockfd, cmd, argp);        // Linux/macOS version
+    return ioctl(sockfd, cmd, argp);
 #endif
 }
 
@@ -738,7 +739,7 @@ void CVoice::HandleNetwork()
 			struct sockaddr_storage addr;
 			socklen_t size = sizeof(addr);
 
-			int Socket = accept(m_ListenSocket, (sockaddr *)&addr, &size);
+			socket_t Socket = accept(m_ListenSocket, (sockaddr *)&addr, &size);
 
 			if (Socket != -1)
 			{
@@ -782,7 +783,7 @@ void CVoice::HandleNetwork()
 					m_aClients[Client].m_UnEven = false;
 
 					m_aPollFds[m_PollFds].fd = Socket;
-					m_aPollFds[m_PollFds].events = POLLIN | POLLHUP;
+					m_aPollFds[m_PollFds].events = POLLIN;
 					m_aPollFds[m_PollFds].revents = 0;
 					m_PollFds++;
 
@@ -853,7 +854,6 @@ void CVoice::HandleNetwork()
 		}
 
 		ssize_t Bytes = recv(pClient->m_Socket, &aBuf[Shift], sizeof(aBuf) - Shift, 0);
-
 		if(Bytes <= 0)
 		{
 			if (pClient->m_Socket != -1)
